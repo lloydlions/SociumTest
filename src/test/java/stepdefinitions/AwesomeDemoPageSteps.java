aed3868cf94601865a8db17d0b1ea2773054161a
Lloyd Lions <lloyd.lions@gmail.com>
2023-05-18 18:55:27 +0800

    change data

--- src/test/java/stepdefinitions/AwesomeDemoPageSteps.java
+++ src/test/java/stepdefinitions/AwesomeDemoPageSteps.java
@@ -42,21 +42,24 @@ public class AwesomeDemoPageSteps {
         /* Count data retrieve, should return only 1 */
         Assert.assertEquals(demoAspAwesomePage.getChildElementCount(new By.ByXPath("(//table[@class='awe-table'])[2]//tr")),1);
 
-        /* Validate : ID */
-        Assert.assertEquals(demoAspAwesomePage.getText(new By.ByXPath("(//table[@class='awe-table'])[2]//td[1]")),"499");
-        /* Validate : Person */
-        Assert.assertEquals(demoAspAwesomePage.getText(new By.ByXPath("(//table[@class='awe-table'])[2]//td[2]")),"Justin");
-        /* Validate : Food */
-        Assert.assertEquals(demoAspAwesomePage.getText(new By.ByXPath("(//table[@class='awe-table'])[2]//td[3]")),"French toast");
-        Assert.assertNotNull(demoAspAwesomePage.getAttribute(new By.ByXPath("(//table[@class='awe-table'])[2]//td[3]//img"), "src"));
-        /* Validate : Country */
-        Assert.assertEquals(demoAspAwesomePage.getText(new By.ByXPath("(//table[@class='awe-table'])[2]//td[4]")),"Hatton Garden");
-        /* Validate : Date */
-        Assert.assertEquals(demoAspAwesomePage.getText(new By.ByXPath("(//table[@class='awe-table'])[2]//td[5]")),"9/17/2010");
-        /* Validate : Meals */
-        Assert.assertEquals(demoAspAwesomePage.getText(new By.ByXPath("(//table[@class='awe-table'])[2]//td[6]")),"Carrot");
-        /* Validate : Chef */
-        Assert.assertEquals(demoAspAwesomePage.getText(new By.ByXPath("(//table[@class='awe-table'])[2]//td[7]")),"Charles Duchemin");
+        for (Map<String, String> dataRow : dataTable) {
+            /* Validate : ID */
+            Assert.assertEquals(demoAspAwesomePage.getText(new By.ByXPath("(//table[@class='awe-table'])[2]//td[1]")),dataRow.get("id"));
+            /* Validate : Person */
+            Assert.assertEquals(demoAspAwesomePage.getText(new By.ByXPath("(//table[@class='awe-table'])[2]//td[2]")),dataRow.get("person"));
+            /* Validate : Food */
+            Assert.assertEquals(demoAspAwesomePage.getText(new By.ByXPath("(//table[@class='awe-table'])[2]//td[3]")),dataRow.get("food"));
+            Assert.assertNotNull(demoAspAwesomePage.getAttribute(new By.ByXPath("(//table[@class='awe-table'])[2]//td[3]//img"), "src"));
+            /* Validate : Country */
+            Assert.assertEquals(demoAspAwesomePage.getText(new By.ByXPath("(//table[@class='awe-table'])[2]//td[4]")),dataRow.get("country"));
+            /* Validate : Date */
+            Assert.assertEquals(demoAspAwesomePage.getText(new By.ByXPath("(//table[@class='awe-table'])[2]//td[5]")),dataRow.get("date"));
+            /* Validate : Meals */
+            Assert.assertEquals(demoAspAwesomePage.getText(new By.ByXPath("(//table[@class='awe-table'])[2]//td[6]")),dataRow.get("meals"));
+            /* Validate : Chef */
+            Assert.assertEquals(demoAspAwesomePage.getText(new By.ByXPath("(//table[@class='awe-table'])[2]//td[7]")),dataRow.get("chef"));
+        }
+
 
         demoAspAwesomePage.driverQuit();
     }
--- src/test/resources/features/DemoPage.feature
+++ src/test/resources/features/DemoPage.feature
@@ -5,9 +5,9 @@ Feature:ASPNET Awesome Demo Page
     And the user navigate to Grid with Filter Row section
     When the user set the following filters
       | columnName | value        |
-      | Person     | Justin       |
+      | Person     | Miguel       |
       | Food       | French toast |
     Then the system will return all the values
-      | id  | person | food         | country       | date      | meals  | chef             |
-      | 499 | Justin | French Toast | Hatton Garden | 9/17/2010 | Carrot | Charles Duchemin |
+      | id  | person | food         | country       | date      | meals  | chef         |
+      | 639 | Miguel | French toast | Stone Cutter  | 4/21/2013 | Rice   | Josh Baskin |
 
